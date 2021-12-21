321da4bd4132b663f9a017ddd22c3afede17fea2
Karol Kurtyka <kurtyka03@gmail.com>
2021-12-21 13:56:14 +0100

    Dodanie Makaronu Bolońskiego

---
+++ app/src/main/java/com/example/pad_pyszne/Restauracje/MakaronyMenu/MakaronyZKrewetkami/MakaronyZKrewetkami.java
@@ -0,0 +1,36 @@
+package com.example.pad_pyszne.Restauracje.MakaronyMenu.MakaronyZKrewetkami;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pad_pyszne.GlownaStrona;
+import com.example.pad_pyszne.Koszyk;
+import com.example.pad_pyszne.MainActivity;
+import com.example.pad_pyszne.R;
+
+public class MakaronyZKrewetkami extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_makarony_zkrewetkami);
+    }
+
+    public void page_Glowna(View view) {
+        Intent i = new Intent(this, GlownaStrona.class);
+        startActivity(i);
+    }
+
+    public void page_Zamownienia(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void page_Koszyk(View view) {
+        Intent i = new Intent(this, Koszyk.class);
+        startActivity(i);
+    }
+}
